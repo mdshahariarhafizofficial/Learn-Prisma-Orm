e0ca5ff660df6adeb0e6deb81300a0023cb40dca
mdshahariarhafizofficial <shahariar.works@gmail.com>
2026-02-07 21:20:45 +0600

    Prisma CRUD Done

--- crud.ts
+++ crud.ts
@@ -23,18 +23,58 @@ const main = async () => {
     // console.log("Post created: ", createPost);
 
     // Retrive all data
-    const retriveAllData = await prisma.user.findMany({
-        // include: {
-        //     posts: true,
-        //     profile: true
-        // }
-        select: {
-            id: true,
-            email: true
+    // const retriveAllData = await prisma.user.findMany({
+    //     // include: {
+    //     //     posts: true,
+    //     //     profile: true
+    //     // }
+    //     select: {
+    //         id: true,
+    //         email: true
+    //     }
+    // });
+    // console.log(retriveAllData);
+
+    // find one
+    // const getUser = await prisma.user.findFirst({
+    //     where: {
+    //         id: 1
+    //     },
+    //     select: {
+    //         posts: true
+    //     }
+    // })
+    // console.log(getUser);
+
+    // // update user
+    // const updateUser = await prisma.user.update({
+    //     where: {
+    //         id: 1
+    //     },
+    //     data: {
+    //         name: "Rahim Uddin"
+    //     }
+        
+    // })
+    // console.log(updateUser);
+
+    // get post
+    const getPost = await prisma.post.findMany({
+        where: {
+            id: 1
         }
-    });
-    console.log(retriveAllData);
-     
+    })
+    console.log(getPost);
+    
+    // delete
+    const deletePost = await prisma.post.delete({
+        where: {
+            id: 1
+        }
+    })
+    console.log(deletePost);
+    
+
 }
 
 main();
